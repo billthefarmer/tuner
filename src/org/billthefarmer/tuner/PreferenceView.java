11807eba3f6b6b173aa1484812dfca75bcc2ad38
Bill Farmer <williamjfarmer@yahoo.co.uk>
2013-02-26 19:26:50 +0000

    Update strobe colour preference

---
+++ src/org/billthefarmer/tuner/PreferenceView.java
@@ -0,0 +1,33 @@
+package org.billthefarmer.tuner;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+public class PreferenceView extends View
+{
+
+	protected static int maxWidth;
+
+	// Constructor
+
+	public PreferenceView(Context context, AttributeSet attrs)
+	{
+		super(context, attrs);
+
+		maxWidth = 0;
+	}
+
+    // On measure
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec)
+    {
+    int w = MeasureSpec.getSize(widthMeasureSpec);
+
+    if (maxWidth < w)
+    	maxWidth = w;
+
+	setMeasuredDimension(maxWidth / 4, maxWidth / 4);
+    }
+}
