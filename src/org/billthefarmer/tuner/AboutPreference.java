e074dfbd9ee6b6aa404219a652c57c9f077c093b
Bill Farmer <williamjfarmer@yahoo.co.uk>
2013-03-02 18:07:38 +0000

    Tidy up about dialog

--- src/org/billthefarmer/tuner/AboutPreference.java
+++ src/org/billthefarmer/tuner/AboutPreference.java
@@ -4,11 +4,12 @@ import android.content.Context;
 import android.preference.DialogPreference;
 import android.util.AttributeSet;
 
-public class AboutPreference extends DialogPreference {
+public class AboutPreference extends DialogPreference
+{
 
-	public AboutPreference(Context context, AttributeSet attrs) {
+	public AboutPreference(Context context, AttributeSet attrs)
+	{
 		super(context, attrs);
-		// TODO Auto-generated constructor stub
 	}
 
 }
