5920ef7bc22b8aaabd0937fd2257b88fb4bdcf40
Bill Farmer <williamjfarmer@yahoo.co.uk>
2023-01-28 12:49:24 +0000

    Update toasts

--- src/main/java/org/billthefarmer/tuner/MainActivity.java
+++ src/main/java/org/billthefarmer/tuner/MainActivity.java
@@ -94,6 +94,8 @@ public class MainActivity extends Activity
 
     private static final int REQUEST_PERMISSIONS = 1;
 
+    public static final int VERSION_CODE_S_V2 = 32;
+
     // Note values for display
     private static final String notes[] =
     {
@@ -660,7 +662,7 @@ public class MainActivity extends Activity
         toast.setGravity(Gravity.CENTER, 0, 0);
         // Fix for android 13
         View view = toast.getView();
-        if (view != null && Build.VERSION.SDK_INT > Build.VERSION_CODES.P)
+        if (view != null && Build.VERSION.SDK_INT > VERSION_CODE_S_V2)
             view.setBackgroundResource(R.drawable.toast_frame);
         toast.show();
 
